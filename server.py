427f681dd1c25e42b78283fa40216ef34d215784
dmarcx <72644283+dmarcx@users.noreply.github.com>
2025-04-24 11:04:24 +0300

    Update server.py

--- server.py
+++ server.py
@@ -27,5 +27,5 @@ def home():
     return "Whisper server is running."
 
 if __name__ == "__main__":
-    port = int(os.environ["PORT"])
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
